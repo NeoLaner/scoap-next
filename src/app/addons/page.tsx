358d9f456ba30f18cf5790ca8f3946f22f52a798
neolaner <neo.laner.official@gmail.com>
2024-05-25 13:35:14 +0330

    add addon button

--- src/app/addons/page.tsx
+++ src/app/addons/page.tsx
@@ -1,7 +1,8 @@
 function Page() {
   return (
     <div className="relative h-full w-full">
-      <button className="bg-solid-green-1 hover:bg-solid-green-2 absolute bottom-2 right-4 rounded-full  p-3 py-1 text-text-gray-2 transition-all">
+      <h1>Addons</h1>
+      <button className="bg-solid-green-1 hover:bg-solid-green-2 absolute bottom-2 right-4 rounded-full p-3 py-1  text-text-gray-2 transition-all md:bottom-2 md:right-6">
         + Addon
       </button>
     </div>
