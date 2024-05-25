d6487746d3d9b98918a0edaf0591f2b4a32ba074
neolaner <neo.laner.official@gmail.com>
2024-05-26 00:08:04 +0330

    add header

--- src/app/addons/page.tsx
+++ src/app/addons/page.tsx
@@ -3,10 +3,16 @@ import InstalledAddons from "./_ui/InstalledAddons";
 
 async function Page() {
   return (
-    <div className="relative h-full w-full">
-      <h1>Addons</h1>
-      <InstalledAddons />
-      <AddAddonModal />
+    <div className="relative h-full w-full px-4 pt-8 md:px-8">
+      <div className="fixed flex h-[48px] w-full items-center">
+        <select className="h-min bg-app-color-gray-2">
+          <option>Installed</option>
+        </select>
+      </div>
+      <div className="pt-[48px]">
+        <InstalledAddons />
+        <AddAddonModal />
+      </div>
     </div>
   );
 }
