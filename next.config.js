1e5d654de7c16a785f4c1bdcf49c3552b7b6a57f
neolaner <neo.laner.official@gmail.com>
2024-05-29 14:58:31 +0330

    update next config

--- next.config.js
+++ next.config.js
@@ -6,6 +6,16 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   images: {
     remotePatterns: [
       {
