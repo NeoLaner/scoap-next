a64deedeffbb8043294acff70935e7095d5b07bc
neolaner <neo.laner.official@gmail.com>
2024-05-30 00:48:36 +0330

    add hls

--- src/app/room/[type]/[imdbId]/[roomId]/_ui/FilePlayer.tsx
+++ src/app/room/[type]/[imdbId]/[roomId]/_ui/FilePlayer.tsx
@@ -48,11 +48,12 @@ function FilePlayer({
   return (
     <>
       <MediaPlayer
-        src={state.mediaSrc}
+        // src={state.mediaSrc}
         // src={{
         //   src: "http://127.0.0.1:11470/f5d61bf3d57082ba2ee1305da5df8dcd10d34539/0",
         //   type: "video/mp4",
         // }}
+        src="http://127.0.0.1:11470/hlsv2/8d64c8c24eecb02a4f60fc5eeeca8393fdafad/master.m3u8?mediaURL=http%3A%2F%2F127.0.0.1%3A11470%2F90b15a09117ec699ee5d335c7fd39a12035dbabc%2F1&videoCodecs=h264&videoCodecs=h265&videoCodecs=hevc&audioCodecs=aac&audioCodecs=mp3&audioCodecs=opus&maxAudioChannels=2"
         autoPlay
         playsInline
         className="text-white ring-media-focus absolute aspect-video h-dvh w-full overflow-hidden bg-blackA12 font-sans data-[focus]:ring-4"
