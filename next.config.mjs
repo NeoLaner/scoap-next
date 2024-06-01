64a61008c71d23589a0680051c7e8ada1d165b2d
neolaner <neo.laner.official@gmail.com>
2024-06-01 11:28:45 +0330

    fix for depl

---
+++ next.config.mjs
@@ -0,0 +1,82 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+await import("./src/env.js");
+
+/** @type {import("next").NextConfig} */
+const config = {
+  output: "standalone",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.metahub.space",
+        port: "",
+        pathname: "/poster/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.metahub.space",
+        port: "",
+        pathname: "/background/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.metahub.space",
+        port: "",
+        pathname: "/logo/**",
+      },
+      {
+        protocol: "https",
+        hostname: "live.metahub.space",
+        port: "",
+        pathname: "/poster/**",
+      },
+      {
+        protocol: "https",
+        hostname: "live.metahub.space",
+        port: "",
+        pathname: "/background/**",
+      },
+      {
+        protocol: "https",
+        hostname: "live.metahub.space",
+        port: "",
+        pathname: "/logo/**",
+      },
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+        port: "",
+        pathname: "/images/**",
+      },
+      //episodes
+      {
+        protocol: "https",
+        hostname: "episodes.metahub.space",
+        port: "",
+        pathname: "/**",
+      },
+      //addons
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
+};
+
+export default config;
