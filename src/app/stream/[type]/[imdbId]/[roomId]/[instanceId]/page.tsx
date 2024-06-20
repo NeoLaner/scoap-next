373965b6fb409cdceffb7ba4245701583760dd5c
neolaner <neo.laner.official@gmail.com>
2024-06-21 02:14:52 +0330

    some bugs

--- src/app/stream/[type]/[imdbId]/[roomId]/[instanceId]/page.tsx
+++ src/app/stream/[type]/[imdbId]/[roomId]/[instanceId]/page.tsx
@@ -7,7 +7,6 @@ async function Page({
   params: { roomId: string; imdbId: string; type: string; instanceId: string };
   searchParams: { season?: string; episode?: string };
 }) {
-
   return (
     <section className="relative h-full w-full overflow-hidden">
       <PlayerLayout params={params} searchParams={searchParams} />
