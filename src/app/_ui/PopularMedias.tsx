aea87430638f023331a96a9de3600e635de86df9
neolaner <neo.laner.official@gmail.com>
2024-10-08 11:44:12 +0330

    add media card

--- src/app/_ui/PopularMedias.tsx
+++ src/app/_ui/PopularMedias.tsx
@@ -1,22 +1,20 @@
+"use client";
 import Image from "next/image";
 import ScrollAreaX from "~/app/_ui/ScrollAreaX";
 import Link from "next/link";
 import { BsImage } from "react-icons/bs";
-import { truncateText } from "~/lib/utils";
+import { cn, truncateText } from "~/lib/utils";
+import { useState } from "react";
 
-function PopularMedias({
-  heading,
-  items,
-}: {
-  heading: string;
-  items: {
-    id: string;
-    imdb_id: string;
-    poster: string | null;
-    name: string;
-    type: string;
-  }[];
-}) {
+type Item = {
+  id: string;
+  imdb_id: string;
+  poster: string | null;
+  name: string;
+  type: string;
+};
+
+function PopularMedias({ heading, items }: { heading: string; items: Item[] }) {
   return (
     <section>
       <div className="flex justify-between">
@@ -25,33 +23,7 @@ function PopularMedias({
       <ScrollAreaX>
         <div className="flex min-w-0 shrink grow basis-0 gap-4 py-6">
           {items.map((item) => (
-            <Link
-              href={`/stream/${item.type}/${item.imdb_id}`}
-              key={item.id}
-              className="flex flex-col items-center gap-4 transition-all hover:scale-105"
-              title={item.name}
-            >
-              <div className="bg-gray-4 md:w-30 relative h-40 w-28 overflow-hidden rounded-lg md:h-44 lg:h-52 lg:w-36">
-                {item.poster && (
-                  <Image
-                    src={item.poster}
-                    alt={item.name}
-                    fill
-                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                    style={{ objectFit: "cover", opacity: 0.9 }}
-                  />
-                )}
-
-                {!item.poster && (
-                  <div className="text-primary-8 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-                    <BsImage size={30} />
-                  </div>
-                )}
-              </div>
-              <div className="max-h-8 max-w-32 overflow-hidden text-center text-xs font-medium">
-                {truncateText(item.name, 28)}
-              </div>
-            </Link>
+            <MediaCard key={item.id} item={item} />
           ))}
         </div>
       </ScrollAreaX>
@@ -60,3 +32,42 @@ function PopularMedias({
 }
 
 export default PopularMedias;
+
+export function MediaCard({ item }: { item: Item }) {
+  const [isHover, setIsHover] = useState(false);
+  return (
+    <Link
+      href={`/stream/${item.type}/${item.imdb_id}`}
+      className="flex flex-col items-center gap-4 transition-all"
+      title={item.name}
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+    >
+      <div className="bg-gray-4 md:w-30 relative h-40 w-28 overflow-hidden rounded-lg md:h-44 lg:h-52 lg:w-36">
+        {item.poster && (
+          <Image
+            src={item.poster}
+            alt={item.name}
+            fill
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            style={{ objectFit: "cover", opacity: 0.9 }}
+            className={cn("transition-all", isHover && "scale-105")}
+          />
+        )}
+
+        {!item.poster && (
+          <div
+            className={cn(
+              "text-primary-8 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2",
+            )}
+          >
+            <BsImage size={30} />
+          </div>
+        )}
+      </div>
+      <div className="max-h-8 max-w-32 overflow-hidden text-center text-xs font-medium">
+        {truncateText(item.name, 28)}
+      </div>
+    </Link>
+  );
+}
