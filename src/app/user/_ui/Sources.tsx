3591b722eec834d5f0a64f5c1a486ddec187e71b
neolaner <neo.laner.official@gmail.com>
2024-11-18 10:05:28 +0330

    update sources

--- src/app/user/_ui/Sources.tsx
+++ src/app/user/_ui/Sources.tsx
@@ -1,11 +1,13 @@
 "use client";
-import { useEffect, useState } from "react";
+import { type CheckedState } from "@radix-ui/react-checkbox";
+import { type Dispatch, type SetStateAction, useEffect, useState } from "react";
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "~/app/_components/ui/accordion";
+import { Button } from "~/app/_components/ui/Button";
 import { Checkbox } from "~/app/_components/ui/checkbox";
 import { IconText } from "~/app/_components/ui/IconComponents";
 import {
@@ -29,9 +31,13 @@ type Srcs = NonNullable<
   Awaited<ReturnType<typeof apiServer.user.getAllSrcByDomain>>
 >["sourcesGroupedByDomain"][number];
 
+type Src = Srcs[string]["srcs"][number];
+
 function Sources() {
   const { data } = api.user.getAllSrcByDomain.useQuery();
   const [domainsStatus, setDomainsStatus] = useState([] as DomainsStatus);
+  const [selectedUrls, setSelectedUrls] = useState([] as Src[]);
+
   const domains = data?.domains;
 
   useEffect(
@@ -81,6 +87,12 @@ function Sources() {
 
   return (
     <div>
+      <div className="flex w-full justify-between">
+        <div>
+          <div>{selectedUrls.length} Selected</div>
+        </div>
+        <Button>Edit</Button>
+      </div>
       {data?.sourcesGroupedByDomain?.map((srcs, i) => {
         return (
           <div key={i}>
@@ -89,11 +101,18 @@ function Sources() {
                 <Accordion key={domain} type="multiple" className="w-full">
                   <AccordionItem value={domain} className="w-full">
                     <Trigger
+                      selectedUrls={selectedUrls}
                       domainsStatus={domainsStatus}
                       srcs={srcs}
                       domain={domain}
+                      setSelectedUrls={setSelectedUrls}
                     />
-                    <Content domain={domain} srcs={srcs} />
+                    <Content
+                      selectedUrls={selectedUrls}
+                      setSelectedUrls={setSelectedUrls}
+                      domain={domain}
+                      srcs={srcs}
+                    />
                   </AccordionItem>
                 </Accordion>
               );
@@ -109,17 +128,52 @@ function Trigger({
   domainsStatus,
   srcs,
   domain,
+  selectedUrls,
+  setSelectedUrls,
 }: {
   domainsStatus: DomainsStatus;
   srcs: Srcs;
   domain: string;
+  selectedUrls: Src[];
+  setSelectedUrls: Dispatch<SetStateAction<Src[]>>;
 }) {
-  const [check, onCheckedChange] = useState(false);
+  const curDomainSrcs = srcs[domain]?.srcs;
+  const curSelectedSrcs = selectedUrls.filter((src) => src.domain === domain);
+  if (!curDomainSrcs) return;
+  const isChecked = curDomainSrcs?.length === curSelectedSrcs.length;
+  const isIndeterminate =
+    !isChecked &&
+    curSelectedSrcs.length !== 0 &&
+    curDomainSrcs?.length > curSelectedSrcs.length;
+  const isUnchecked =
+    !isChecked && curDomainSrcs?.length > curSelectedSrcs.length;
+  const checked = (function (): CheckedState | undefined {
+    if (isChecked) return true;
+    if (isIndeterminate) return "indeterminate" as const;
+    if (isUnchecked) return false;
+  })();
+
   return (
     <div className="flex w-full items-center gap-2">
       <Checkbox
-        checked={check}
-        onCheckedChange={(prv) => onCheckedChange(Boolean(prv))}
+        checked={checked}
+        onClick={() => {
+          if (checked)
+            setSelectedUrls((prv) => {
+              return prv.filter((src) => src.domain !== domain);
+            });
+          else
+            setSelectedUrls((prv) => {
+              const removeAllSelected = prv.filter(
+                (src) => src.domain !== domain,
+              );
+              const selectedSrcs = srcs[domain];
+              if (selectedSrcs)
+                return [...removeAllSelected, ...selectedSrcs.srcs];
+              else return [...prv];
+            });
+        }}
+        // onCheckedChange={(prv) => onCheckedChange(Boolean(prv))}
       />
       <AccordionTrigger className="w-full justify-normal gap-4 text-muted-foreground">
         <TooltipProvider>
@@ -163,7 +217,17 @@ function Trigger({
   );
 }
 
-function Content({ srcs, domain }: { srcs: Srcs; domain: string }) {
+function Content({
+  srcs,
+  domain,
+  selectedUrls,
+  setSelectedUrls,
+}: {
+  srcs: Srcs;
+  domain: string;
+  selectedUrls: Src[];
+  setSelectedUrls: Dispatch<SetStateAction<Src[]>>;
+}) {
   return (
     <AccordionContent className="space-y-1">
       {srcs?.[domain]?.srcs.map((src) => {
@@ -176,7 +240,18 @@ function Content({ srcs, domain }: { srcs: Srcs; domain: string }) {
             })}
             className="ml-4 flex items-center gap-2"
           >
-            <Checkbox />
+            <Checkbox
+              checked={
+                selectedUrls.filter((selSrc) => selSrc.id === src.id).length > 0
+              }
+              onCheckedChange={(checked) => {
+                if (checked === true) setSelectedUrls((prv) => [...prv, src]);
+                if (checked === false)
+                  setSelectedUrls((prv) => {
+                    return prv.filter((source) => source.id !== src.id);
+                  });
+              }}
+            />
             <div className="flex items-center gap-4 text-muted-foreground">
               <div>
                 <IconText>{src.type === "media" ? "med" : "sub"}</IconText>{" "}
--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,13 +1,16 @@
 import { api } from "~/trpc/server";
-import Test from "./_ui/Sources";
+import Sources from "./_ui/Sources";
+
+import { getServerAuthSession } from "~/server/auth";
 
 async function page() {
+  const user = await getServerAuthSession();
+  if (!user?.user.id) return <div>Login</div>;
   const srcs = await api.user.getAllSrcByDomain();
-  console.log(srcs);
 
   return (
     <div>
-      <Test />
+      <Sources />
     </div>
   );
 }
